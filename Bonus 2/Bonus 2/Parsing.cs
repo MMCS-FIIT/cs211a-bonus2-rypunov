a122c463969f8c89d0bbd242a34f2374d9917a9d
Денис Рыпунов <rypunov2005@bk.ru>
2024-05-03 00:23:57 +0300

    Добавлен метод для парсинга файла с фильмами

--- Bonus 2/Bonus 2/Parsing.cs
+++ Bonus 2/Bonus 2/Parsing.cs
@@ -45,5 +45,34 @@ namespace Bonus_2
                         "\nЖанр - " + Genre + "\nТип - " + Type + "\nКол-во эпизодов - " + TotalDuration +  "\n";
             }
         }
+
+        /// <summary>
+        /// Обрабатывает файл для хранения информации о фильмах
+        /// </summary>
+        public static void Parssing()
+        {
+            var lines_arr = System.IO.File.ReadAllLines("input_files/imdb.csv");
+            var movie_database = new List<Film>();
+
+            for (int i = 0; i < lines_arr.Count(); i++)
+            {
+                var reg = Regex.Matches(lines_arr[i], @""".+?""").ToArray();
+                string[] el_film = lines_arr[i].Split(new char[] { ',' });
+                var genre = "";
+                if (reg.Count() < 5)
+                    genre = el_film[4];
+                else
+                    genre = reg[3].ToString();
+
+                if (el_film[2] != "No Rate")
+                {
+                    movie_database.Add(new Film(el_film[0], el_film[1],
+                        double.Parse(el_film[2].Replace('.', ',')), genre,
+                        el_film[^9], el_film[^8], el_film[^6]));
+                }
+            }
+            //for (int i = 0; i < 50; i++)
+            //    Console.WriteLine(movie_database[i]);
+        }
     }
 }
