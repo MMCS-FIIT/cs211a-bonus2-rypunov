44294ab89059b3a2b86cad5d1671b774e916ef82
Денис Рыпунов <rypunov2005@bk.ru>
2024-05-02 23:47:37 +0300

    Добавлен класс для инициализации фильма

---
+++ Bonus 2/Bonus 2/Parsing.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Bonus_2
+{
+    internal class Parsing
+    {
+        /// <summary>
+        /// Класс фильма
+        /// </summary>
+        class Film
+        {
+            public string Name { get; }
+            public string Date { get; }
+            public double Rating { get; }
+            public string Genre { get; }
+            public string Type { get; }
+            public int TotalDuration { get; }
+            public Film(string name, string date, double raiting, string genre, string duration, string type, string episodes)
+            {
+                Name = name;
+                Date = date;
+                Rating = raiting;
+                Genre = genre;
+                Type = type;
+                if (type == "Film")
+                    TotalDuration = int.Parse(duration);
+                else
+                    TotalDuration = int.Parse(episodes);
+            }
+
+            public override string ToString()
+            {
+                if (Type == "Film")
+                    return "Название - " + Name + "\" \nГод выпуска - " + Date + "\nРейтинг - " + Rating + 
+                        "\nЖанр - " + Genre + "\nТип - " + Type + "\nПродолжительность - " + TotalDuration + "\n";
+                else
+                    return "Название - " + Name + "\" \nГод выпуска - " + Date + "\nРейтинг - " + Rating +
+                        "\nЖанр - " + Genre + "\nТип - " + Type + "\nКол-во эпизодов - " + TotalDuration +  "\n";
+            }
+        }
+    }
+}
--- Bonus 2/Bonus 2/Program.cs
+++ Bonus 2/Bonus 2/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace SimpleTGBot;
+﻿using Bonus_2;
+
+namespace SimpleTGBot;
 
 public static class Program
 {
     // Метод main немного видоизменился для асинхронной работы
     public static async Task Main(string[] args)
     {
+        //Parsing.Parssing();
         TelegramBot telegramBot = new TelegramBot();
         await telegramBot.Run();
     }
